053b2f64de9f940c4bafd62e6f4248c75089bd07
Igor <kravchenkoigor010687@gmail.com>
2023-02-08 10:02:53 +0100

    1 st folder create

---
+++ task0/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+Console.WriteLine($"Сумма цифр = {GetSumNums(num1)}");
+Console.WriteLine($"Сумма цифр = {GetSumNums(num2)}");
+
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
+}

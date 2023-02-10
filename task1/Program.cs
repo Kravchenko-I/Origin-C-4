0f1d09abf7a830821798eb16ecb621400010a62f
Igor <kravchenkoigor010687@gmail.com>
2023-02-10 19:47:30 +0100

    Create 1st folder

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр: {GetCount(num)}");
+int GetCount(int number)
+{
+    int count = 1;
+    while (number > 10)
+    {
+        number = number / 10;
+        count = count + 1;
+    }
+    return count;
+}

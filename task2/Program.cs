e48e60a0836c170c64c2646f2b74d647f1bcbb5f
Igor <kravchenkoigor010687@gmail.com>
2023-02-10 19:47:44 +0100

    Create 2nd folder

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел = {GetMultiply(num)}");
+int GetMultiply(int number)
+{
+    int temp = 1;
+    for (int count = 1; count <= number ; count++) 
+    {
+        temp = temp * count;
+    }
+    return temp;
+}
+
